ca2d998e3b10ccf9183daa712ea130f16abaa739
Wen Tjun <wentjun289@hotmail.com>
2018-12-30 20:22:27 +0800

    added responsiveness method
    - replaced reliance on view box and preserveAspectRatio with custom
    responsiveness method

--- docs/chart.js
+++ docs/chart.js
@@ -32,6 +32,35 @@ loadData.then(data => {
   initialiseChart(data);
 });
 
+// credits: https://brendansudol.com/writing/responsive-d3
+const responsivefy = svg => {
+  // get container + svg aspect ratio
+  const container = d3.select(svg.node().parentNode),
+    width = parseInt(svg.style('width')),
+    height = parseInt(svg.style('height')),
+    aspect = width / height;
+
+  // get width of container and resize svg to fit it
+  const resize = () => {
+    var targetWidth = parseInt(container.style('width'));
+    svg.attr('width', targetWidth);
+    svg.attr('height', Math.round(targetWidth / aspect));
+  };
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg
+    .attr('viewBox', '0 0 ' + width + ' ' + height)
+    .attr('perserveAspectRatio', 'xMinYMid')
+    .call(resize);
+
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on('resize.' + container.attr('id'), resize);
+};
+
 const initialiseChart = data => {
   data = data.filter(
     row => row['high'] && row['low'] && row['close'] && row['open']
